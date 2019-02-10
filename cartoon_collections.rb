7d6312fd7febbcd538de909456b04f93008f6a56
Ali Madni <ali555m@hotmail.com>
2019-02-10 20:29:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,7 +24,7 @@ def find_the_cheese(array)
     array.map do |val|
       if val == "cheddar" || val =="gouda" || val == "camembert"
         return val
-      end        
+      end
       end
     end
   else
