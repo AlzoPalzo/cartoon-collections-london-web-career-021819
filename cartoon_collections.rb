6f54caeeb94bbacb146f5e963436c5b0305b3734
Ali Madni <ali555m@hotmail.com>
2019-02-10 19:49:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,11 @@ def roll_call_dwarves(dwarves)
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+  calls.map do |call| 
+    call.capitalize
+    call << !
+  end
 end
 
 def long_planeteer_calls# code an argument here
